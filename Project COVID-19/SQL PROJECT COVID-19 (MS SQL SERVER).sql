537755efd3203d4d8a768aec2a06ae5322f4debe
Piyachai Bowornthammakit <128880659+PiyachaiBowornthammakit@users.noreply.github.com>
2023-06-16 14:20:14 +0700

    Update SQL PROJECT COVID-19 (MS SQL SERVER).sql

--- Project COVID-19/SQL PROJECT COVID-19 (MS SQL SERVER).sql
+++ Project COVID-19/SQL PROJECT COVID-19 (MS SQL SERVER).sql
@@ -130,7 +130,7 @@ SELECT *,
 	(total_new_vaccinations/population)*100 AS percentage_new_vaccine
 FROM thailand_vac;
 
-
+--Query for Dashboard (Tableau Public)
 --1
 Select
 	location,
@@ -139,30 +139,36 @@ Select
 	SUM(cast(new_deaths as int)) as total_deaths,
 	SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From Project..CovidDeaths
---Where location like '%states%'
 where continent is not null 
 Group By location, continent
 order by 1,2,3
 
 --2
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+Select 
+	location,
+	SUM(cast(new_deaths as int)) as TotalDeathCount
 From Project..CovidDeaths
---Where location like '%states%'
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
 Group by location
 order by TotalDeathCount desc
 
 --3
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+Select 
+	Location,
+	Population, MAX(total_cases) as HighestInfectionCount,
+	Max((total_cases/population))*100 as PercentPopulationInfected
 From Project..CovidDeaths
---Where location like '%states%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
 --4
-Select Location, continent, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+Select 
+	Location,
+	continent,
+	Population,date,
+	MAX(total_cases) as HighestInfectionCount,
+	Max((total_cases/population))*100 as PercentPopulationInfected
 From Project..CovidDeaths
---Where location like '%states%'
 Group by Location, Population, date, continent
 order by PercentPopulationInfected desc
